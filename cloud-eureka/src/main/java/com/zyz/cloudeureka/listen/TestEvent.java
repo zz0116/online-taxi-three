1a2e03475b18dc2a80cbf96de1650f7b00f286b5
张远卓 <zhangyuanzhuo@tcld.com.cn>
2020-10-14 11:13:49 +0800

    eureka源码1

--- cloud-eureka/src/main/java/com/zyz/cloudeureka/listen/TestEvent.java
+++ cloud-eureka/src/main/java/com/zyz/cloudeureka/listen/TestEvent.java
@@ -1,13 +1,13 @@
 package com.zyz.cloudeureka.listen;
 
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 @Component
 public class TestEvent {
+
+    @EventListener
     public void listen(EurekaInstanceCanceledEvent e) {
         // 发邮件 短信
         System.out.println("下线： " + e.getServerId());
