4abeaecf07f37aa9db6060ec2e478a8cddafca6b
张远卓 <zyz9255@gmail.com>
2020-10-14 23:31:50 +0800

    三级缓存演示

--- api-passenger/src/main/java/com/zyz/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/com/zyz/apipassenger/ApiPassengerApplication.java
@@ -1,9 +1,18 @@
 package com.zyz.apipassenger;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RestController
@@ -18,4 +27,32 @@ public class ApiPassengerApplication {
         return "api-passenger";
     }
 
+    /**
+     * 演示三级缓存 guava
+     */
+    LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(5, TimeUnit.SECONDS)
+            .build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String key) throws Exception {
+                    return "load: " + new Random().nextInt(100);
+                }
+            });
+
+    @PostMapping("/test-set/{id}")
+    public String testSet(@PathVariable("id") String id) {
+        cache.put(id, "set: " + new Random().nextInt(100));
+        return "success";
+    }
+
+    @GetMapping("/test-get/{id}")
+    public String testGet(@PathVariable("id") String id) {
+        String value = null;
+        try {
+            value = cache.get(id);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
 }
